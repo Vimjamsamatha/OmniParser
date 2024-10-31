3f214da3d1733006ce99b9a6e0fff0c3e71fc730
Vimjamsamatha <samathavimjam@gmail.com>
2024-10-31 12:37:50 +0530

    Update convert_safetensor_to_pt.py

--- weights/convert_safetensor_to_pt.py
+++ weights/convert_safetensor_to_pt.py
@@ -3,7 +3,7 @@ from ultralytics.nn.tasks import DetectionModel
 from safetensors.torch import load_file
 // iam here to learn about comit conflicts
 tensor_dict = load_file("weights/icon_detect/model.safetensors")
-
+- this is an example
 model = DetectionModel('weights/icon_detect/model.yaml')
 model.load_state_dict(tensor_dict)
 torch.save({'model':model}, 'weights/icon_detect/best.pt')
